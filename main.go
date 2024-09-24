a8db353c9558d2da7ce107c678a1948b2ca99491
agshinaliyev <124487682+agshinaliyev@users.noreply.github.com>
2024-09-25 00:47:29 +0400

    Update main.go

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 import "fmt"
 
 
-func main() 
-{
+func main() {
 	fmt.Println("Welcome to the Task Management System")
 	fmt.Println("Project start date is: 2024-09-18 00:00:00")
 	fmt.Println("Task Management System")
