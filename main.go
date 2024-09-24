fc3551d8e159905821695421dd0c860596e20bd4
agshinaliyev <aqsin136@gmail.com>
2024-09-24 22:22:17 +0400

    session1/task

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello and welcome")
+	fmt.Println(" to the Task Management System")
+	fmt.Println("Project start date is: 2024-09-18 00:00:00")
+	fmt.Println("Task Management System")
 
 }
